a9f8333a52f06019c267afd0121e8e2beba59c75
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-04-06 00:18:17 -0700

    Add all necessary features in components to main

--- my-first-alx-project/src/Landingpage/landingPage.jsx
+++ my-first-alx-project/src/Landingpage/landingPage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Header from './components/navBar'
+import Hero from './components/hero'
+import About from './components/about'
+import WhyJoin from './components/whyjoin'
+import Blog from './components/blog'
+import Team from './components/theme'
+import Footer from './components/footer'
 
 const LandingPage = () => {
   return (
-    <div>
-      Landing Page
-    </div>
+    <main className='flex flex-col w-full font-Poppins'>
+      <Header/>
+      <Hero/>
+      <About/>
+      <WhyJoin/>
+      <Blog/>
+      <Team/>
+      <Footer/>
+    </main>
   )
 }
 
