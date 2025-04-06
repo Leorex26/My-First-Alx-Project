c0b145fce3538dae0a2a7e6cb739fbab6e7c9fa8
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-04-06 06:16:29 -0700

    finished components for navbar and hooks for useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
+import NavBar from './components/navbar'
+import { Outlet } from 'react-router-dom'
+import SearchBar from './components/searchbar'
 
 function App() {
-  
-
   return (
-    <>
-      <p className='text-md bg-Leo'>Hello World</p>
-      
-    </>
+    <main className='flex flex-col items-center gap-5 font-Poppins md:gap-10'> 
+      <SearchBar />
+      <section className='flex flex-col w-full sm:border px-[5%] border-gray-200 sm:max-w-[80%] lg:max-w-[60%]'>
+        <NavBar />
+        <Outlet />  
+      </section>
+    </main>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
+import { StrictMode } from "react";
+import Latest from "./components/latest.jsx";
+import { createRoot } from "react-dom/client";
+import Explore from "./components/explore.jsx";
+import Trending from "./components/trending.jsx";
+import LandingPage from "./Landingpage/landingPage.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from './Landingpage/landingPage.jsx';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="home" element={<App />}/>
-        
+        <Route path="news" element={<App />}>
+          <Route path="latest" element={<Latest />} />
+          <Route path="explore" element={<Explore />} />  
+          <Route path="trending" element={<Trending />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
